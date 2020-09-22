9843fb2c441a2e1ea13fe37fbf7f26517e44606c
Marcin Kik <mki1967@gmail.com>
2020-09-22 10:19:13 +0200

    using `terser` to merge and minify scripts.

--- list-files-to-cache.bash
+++ list-files-to-cache.bash
@@ -2,7 +2,8 @@ ls -1 mkisg.html;
 ls -1 pwa-manifest.json;
 # ls -1 styles.css;
 # ls -1 ga.js ;
-ls -1 js/*.js;
+# ls -1 js/*.js; # separate files
+ls -1 terser/*.js; # one minified file
 ls -1 assets/mki3d/index.json;
 ls -1 assets/mki3d/tokens/*.mki3d;
 ls -1 assets/mki3d/stages/*.mki3d;
--- make-files.bash
+++ make-files.bash
@@ -1,1 +1,10 @@
+# USE THIS SCRIPT BEFORE COMMITING
+
+# prepare minified script file with `terser`.
+# (Install `terser` with: `npm install terser -g` )
+# (See: `https://github.com/terser/terser` )
+
+terser js/*.js > terser/minified.js
+
+# prepare files to be cached by service-worker
 ./make-files-tocache.bash
